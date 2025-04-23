1ae42842d62bad8d2b8041b2ab4f225dd36f6d67
gboy8462 <carlosragudo8462@gmail.com>
2025-04-23 09:26:21 -0700

    update

--- Finals.py
+++ Finals.py
@@ -4,8 +4,8 @@ from tkinter import ttk
 # Group 2 ITCS103 Finals Project
 
 window = tk.Tk()
-window.geometry("500x500")
-window.title("Group2 Finals Project!")
+window.geometry("700x500")
+window.title("Quiz App!")
 
 # widgets
 
