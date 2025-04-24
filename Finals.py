9a618681f466278b0acf85b834eab5cb567e9294
gb8462 <planetes8462@gmail.com>
2025-04-24 20:40:33 +0800

    update

--- Finals.py
+++ Finals.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from Modules import (Home)
+from Modules import Home
 
 window = tk.Tk()
 window.geometry("700x500")
@@ -9,12 +9,10 @@ window.configure(bg="#353A3E")
 
 # Functions
 def handle_click(event):
-    Home.home()
+    Home.home(window)  # Pass the window to the home function
 
 # widgets
-
-button = tk.Button(window, text="Login")
-
+button = tk.Button(window, text="Testing")
 button.bind("<Button-1>", handle_click)
 button.pack(pady=100)
 
