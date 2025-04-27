02796ba7b01cebc478951b3c5c455378262fe8d1
gb8462 <planetes8462@gmail.com>
2025-04-27 12:07:55 -0700

    update

--- QuizApp.py
+++ QuizApp.py
@@ -7,6 +7,12 @@ customtkinter.set_appearance_mode("dark")
 
 main.configure(fg_color="#010101")
 
+topbar = customtkinter.CTkFrame(main, fg_color="#252525",corner_radius=0,height=60)
+topbar.pack(pady=0, padx=10, fill="both")
+
+background = customtkinter.CTkFrame(main, fg_color="#E0E0E0",corner_radius=0,height=400)
+background.pack(pady=(0,10), padx=10, fill="both", expand=True)
+
 # Dashboard
 
 
