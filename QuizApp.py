359985bc35720d5a585b1aa163af1f0329cce894
gb8462 <planetes8462@gmail.com>
2025-04-27 10:24:41 -0700

    update

--- QuizApp.py
+++ QuizApp.py
@@ -2,16 +2,24 @@ import customtkinter
 
 main = customtkinter.CTk()
 main.title("TryQuizMe")
-main.geometry("1090x680")
+main.geometry("1100x680")
 customtkinter.set_appearance_mode("dark")
 
 main.configure(fg_color="#010101")
 
-sideboard = customtkinter.CTkFrame(main, corner_radius=10,fg_color='#1f2024')
-sideboard.pack(pady=(5,20), padx=(0,10), side='right',fill="both", expand=True)
 
-dashboard = customtkinter.CTkFrame(main, corner_radius=10,fg_color='#edeff0',width=300)
-dashboard.pack(pady=(5,20), padx=10, fill="both",side='right')
+dashboard = customtkinter.CTkFrame(main, corner_radius=10,fg_color='#dee0e0',width=300)
+dashboard.pack(pady=(5,20), padx=10, fill="both",side='left')
+
+label = customtkinter.CTkLabel(dashboard,text="QuizMe",font=('Arial',35), text_color='#101010')
+label.pack(pady=(15,15), padx=70)
+
+label = customtkinter.CTkLabel(dashboard,text="Difficulty",font=('Arial',18), text_color='#101010')
+label.pack(pady=(30,5), padx=70)
+
+
+sideboard = customtkinter.CTkFrame(main, corner_radius=10,fg_color='#dee0e0')
+sideboard.pack(pady=(5,20), padx=(0,10), side='right',fill="both", expand=True)
 
 
 main.mainloop()
