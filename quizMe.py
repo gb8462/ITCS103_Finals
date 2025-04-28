7eaf89772719c4047389df42f7a6feb7b128aa6a
gb8462 <planetes8462@gmail.com>
2025-04-27 22:42:29 -0700

    update

--- quizMe.py
+++ quizMe.py
@@ -15,45 +15,46 @@ label = customtkinter.CTkLabel(dashboard,text="QuizMe",font=('Arial',35), text_c
 label.pack(pady=(15,15), padx=70)
 
 label = customtkinter.CTkLabel(dashboard,text="Difficulty",font=('Arial',17), text_color='#101010')
-label.pack(pady=(25,0), padx=70)
+label.pack(pady=(25,0), padx=(0,135))
 
 
 # Difficulty
-difficulty = customtkinter.CTkFrame(dashboard, fg_color='#ebeded', border_width=1, border_color='#c9c9c9',height=230)
+difficulty = customtkinter.CTkFrame(dashboard, fg_color='#ebeded', border_width=1, border_color='#c9c9c9', height=230)
 difficulty.pack()
 
-button = customtkinter.CTkButton(difficulty, text="All Difficulty", corner_radius=0, width=200,fg_color='#ffffff', text_color='#101010')
+button = customtkinter.CTkButton(difficulty, text="All Difficulty", anchor='w', height=35,corner_radius=0,width=200,fg_color='#ffffff', text_color='#101010')
 button.pack(pady=1, padx=1)
 
-button = customtkinter.CTkButton(difficulty, text="Easy", corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
+button = customtkinter.CTkButton(difficulty, text="Easy", anchor='w', height=35, corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
 button.pack(pady=1, padx=1)
 
-button = customtkinter.CTkButton(difficulty, text="Medium", corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
+button = customtkinter.CTkButton(difficulty, text="Medium", anchor='w', height=35, corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
 button.pack(pady=1, padx=1)
 
-button = customtkinter.CTkButton(difficulty, text="Hard", corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
+button = customtkinter.CTkButton(difficulty, text="Hard", anchor='w', height=35, corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
 button.pack(pady=1, padx=1)
 
-label = customtkinter.CTkLabel(dashboard,text="Category",font=('Arial',17), text_color='#101010')
-label.pack(pady=(25,0), padx=70)
 
 # Category
+label = customtkinter.CTkLabel(dashboard,text="Category",font=('Arial',17), text_color='#101010')
+label.pack(pady=(25,0), padx=(0,135))
+
 category = customtkinter.CTkFrame(dashboard, fg_color='#ebeded', border_width=1, border_color='#c9c9c9',height=230)
 category.pack(pady=0, padx=1)
 
-button = customtkinter.CTkButton(category, text="All Category", corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
+button = customtkinter.CTkButton(category, text="All Category", anchor='w', height=35, corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
 button.pack(pady=1, padx=1)
 
-button = customtkinter.CTkButton(category, text="General Skills", corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
+button = customtkinter.CTkButton(category, text="General Skills", anchor='w', height=35, corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
 button.pack(pady=1, padx=1)
 
-button = customtkinter.CTkButton(category, text="Web Development", corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
+button = customtkinter.CTkButton(category, text="Web Development", anchor='w', height=35, corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
 button.pack(pady=1, padx=1)
 
-button = customtkinter.CTkButton(category, text="Cryptography", corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
+button = customtkinter.CTkButton(category, text="Cryptography", anchor='w', height=35, corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
 button.pack(pady=1, padx=1)
 
-button = customtkinter.CTkButton(category, text="Python", corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
+button = customtkinter.CTkButton(category, text="Python", anchor='w', height=35, corner_radius=0, width=200, fg_color='#ffffff', text_color='#101010')
 button.pack(pady=1, padx=1)
 
 # Quizzes
